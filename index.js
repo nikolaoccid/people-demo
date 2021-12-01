6c261b98d6331c5115e99d2a55181820ccddea87
Nikola Occidentale <nikolaoccid@gmail.com>
2021-12-01 14:11:08 +0100

    Add delete functionality

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/api/people/new', (req,res) => {
   const oib = req.query.oib;
   const peopleString = fs.readFileSync('people.json', 'utf8');
   const people = JSON.parse(peopleString);
-  const id = people.length + 1;
+  const id = people.slice(-1)[0].id + 1;
   people.push({id: id, name: name, last_name: lastName, oib: oib});
   const newPeopleString = JSON.stringify(people, null, 2);
   fs.writeFileSync('people.json', newPeopleString) ;
@@ -29,6 +29,16 @@ app.get('/people/new', (req, res) =>{
 app.get('/people', (req, res) =>{
   const peopleTableHTML = fs.readFileSync('people-table.html', 'utf8');
   res.send(peopleTableHTML);
+
+})
+app.get('/api/people/delete', (req, res) =>{
+  const id = parseInt(req.query.id);
+  const peopleString = fs.readFileSync('people.json', 'utf8');
+  const people = JSON.parse(peopleString);
+  const newPeople = people.filter(item => item.id !== id)
+  const newPeopleString = JSON.stringify(newPeople, null, 2);
+  fs.writeFileSync('people.json', newPeopleString);
+  res.send("Successfully deleted record!");
 
 })
 app.listen(port, () => {
