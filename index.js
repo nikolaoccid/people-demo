fbf39e53df9a1b27c6d8d0f6b067b3dc8826d2c7
Nikola Occidentale <nikolaoccid@gmail.com>
2021-12-04 22:38:36 +0100

    Clean up the codebase

--- index.js
+++ index.js
@@ -2,75 +2,59 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3000;
+const io = require('./json-io')
 
 app.get('/', (req, res) => {
   res.redirect('/people');
 })
 app.get('/api/people', (req, res)=>{
-  const peopleString = fs.readFileSync('people.json', 'utf8');
-  res.send(peopleString);
+  res.send(io.readJSON('people.json'));
 })
 app.get('/api/people/new', (req,res) => {
   const name= req.query.name;
   const lastName = req.query.last_name;
   const oib = req.query.oib;
-  const peopleString = fs.readFileSync('people.json', 'utf8');
-  const people = JSON.parse(peopleString);
+  const people = io.readJSON('people.json');
   const id = people.slice(-1)[0].id + 1;
   people.push({id: id, name: name, last_name: lastName, oib: oib});
-  const newPeopleString = JSON.stringify(people, null, 2);
-  fs.writeFileSync('people.json', newPeopleString) ;
+  io.writeJSON('people.json', people);
   res.send("Successfully done!");
 })
 app.get('/people/new', (req, res) =>{
-  const formHTML= fs.readFileSync('form.html', 'utf-8');
-  res.send(formHTML);
+  res.send(fs.readFileSync('form.html', 'utf-8'));
 })
 app.get('/people', (req, res) =>{
-  const peopleTableHTML = fs.readFileSync('people-table.html', 'utf8');
-  res.send(peopleTableHTML);
+  res.send(fs.readFileSync('people-table.html', 'utf8'));
 
 })
 app.get('/api/people/delete', (req, res) =>{
   const id = parseInt(req.query.id);
-  const peopleString = fs.readFileSync('people.json', 'utf8');
-  const people = JSON.parse(peopleString);
+  const people = io.readJSON('people.json');
   const newPeople = people.filter(item => item.id !== id)
-  const newPeopleString = JSON.stringify(newPeople, null, 2);
-  fs.writeFileSync('people.json', newPeopleString);
+  io.writeJSON('people.json', newPeople);
   res.send("Successfully deleted record!");
-
 })
 app.get('/api/people/edit', (req, res) =>{
   const id = parseInt(req.query.id);
   const name = req.query.name;
   const lastName = req.query.last_name;
   const oib = req.query.oib;
-  const peopleString = fs.readFileSync('people.json', 'utf8');
-  const people = JSON.parse(peopleString);
-
+  const people = io.readJSON('people.json');
   const index = people.findIndex(person => person.id === id);
   people[index].name = name;
   people[index].last_name = lastName;
   people[index].oib = oib;
-
-  const newPeople = JSON.stringify(people, null, 2);
-  fs.writeFileSync('people.json',newPeople);
+  io.writeJSON('people.json', people);
   res.send("Successfully edited the record.");
 })
 app.get('/api/people/:id', (req, res) =>{
   const id = parseInt(req.params.id);
-  function isRequestedPerson(person){
-    return person.id === id;
-  }
-  const peopleString = fs.readFileSync('people.json', 'utf8');
-  const people = JSON.parse(peopleString);
-  const person = people.find(isRequestedPerson);
+  const people = io.readJSON('people.json');
+  const person = people.find(person => person.id === id);
   res.send(person);
 })
 app.get('/people/edit', (req, res) => {
-  const formHTML = fs.readFileSync('form.html', 'utf8');
-  res.send(formHTML);
+  res.send(fs.readFileSync('form.html', 'utf8'));
 })
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
---
+++ json-io.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+function readJSON(filename){
+  const file = fs.readFileSync(filename, 'utf8');
+  return JSON.parse(file);
+}
+
+function writeJSON(filename, data){
+  const serializedData = JSON.stringify(data, null, 2);
+  fs.writeFileSync(filename, serializedData);
+}
+exports.readJSON = readJSON;
+exports.writeJSON = writeJSON;
